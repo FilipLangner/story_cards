dc1849bd8978878f5b00795046f023954aba9da4
FilipLangner <filip.langner@gmail.com>
2020-04-04 16:58:07 +0200

    Adding images to flashcards

--- Final_project/urls.py
+++ Final_project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
@@ -25,5 +27,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('story_cards/', include('story_cards.urls', namespace="story-cards")),
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- story_cards/views.py
+++ story_cards/views.py
@@ -168,3 +168,8 @@ class PlayStoryCardsView(generic.ListView):
         random.shuffle(flashcard_list)
         flashcard_subset = flashcard_list[:3]
         return flashcard_subset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['deck'] = Deck.objects.get(id=self.kwargs.get('deck_id'))
+        return context
